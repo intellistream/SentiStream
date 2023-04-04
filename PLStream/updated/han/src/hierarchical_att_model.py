8f9c4c0f43379755af0dcb7e18c6cc37c118cb6c
Karthick47v2 <Karthicksharma11@gmail.com>
2023-04-04 20:28:14 +0800

    refactor

--- PLStream/updated/han/src/hierarchical_att_model.py
+++ PLStream/updated/han/src/hierarchical_att_model.py
@@ -13,8 +13,10 @@ class HAN(nn.Module):
         self.sent_hidden_size = sent_hidden_size
         self.max_sent_length = max_sent_length
         self.max_word_length = max_word_length
-        self.word_att_net = WordAttNet(pretrained_word2vec_path, word_hidden_size)
-        self.sent_att_net = SentAttNet(sent_hidden_size, word_hidden_size, num_classes)
+        self.word_att_net = WordAttNet(
+            pretrained_word2vec_path, word_hidden_size)
+        self.sent_att_net = SentAttNet(
+            sent_hidden_size, word_hidden_size, num_classes)
         self._init_hidden_state()
 
     def _init_hidden_state(self, last_batch_size=None):  # rm last_batch_size
@@ -22,8 +24,10 @@ class HAN(nn.Module):
             batch_size = last_batch_size
         else:
             batch_size = self.batch_size
-        self.word_hidden_state = torch.zeros(2, batch_size, self.word_hidden_size)
-        self.sent_hidden_state = torch.zeros(2, batch_size, self.sent_hidden_size)
+        self.word_hidden_state = torch.zeros(
+            2, batch_size, self.word_hidden_size)
+        self.sent_hidden_state = torch.zeros(
+            2, batch_size, self.sent_hidden_size)
         if torch.cuda.is_available():
             self.word_hidden_state = self.word_hidden_state.cuda()
             self.sent_hidden_state = self.sent_hidden_state.cuda()
@@ -33,9 +37,11 @@ class HAN(nn.Module):
         output_list = []
         input = input.permute(1, 0, 2)
         for i in input:
-            output, self.word_hidden_state = self.word_att_net(i.permute(1, 0), self.word_hidden_state)
+            output, self.word_hidden_state = self.word_att_net(
+                i.permute(1, 0), self.word_hidden_state)
             output_list.append(output)
         output = torch.cat(output_list, 0)
-        output, self.sent_hidden_state = self.sent_att_net(output, self.sent_hidden_state)
+        output, self.sent_hidden_state = self.sent_att_net(
+            output, self.sent_hidden_state)
 
         return output
