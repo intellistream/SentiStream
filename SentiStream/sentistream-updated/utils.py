19df976398f9975c8ff561b6d7defa8829a90c5b
karthick47v2 <karthicksharma11@gmail.com>
2023-04-14 22:06:32 +0800

    refactor

--- SentiStream/sentistream-updated/utils.py
+++ SentiStream/sentistream-updated/utils.py
@@ -1,71 +1,88 @@
-from nltk.stem import SnowballStemmer
-import torch
-from nltk.tokenize import sent_tokenize, word_tokenize
-from gensim.utils import simple_preprocess
-# from gensim.parsing.preprocessing import remove_stopwords
-import pandas as pd
-import os
+# pylint: disable=import-error
+
 import random
 import re
 import numpy as np
 import nltk
+import torch
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import word_tokenize
+
 
 nltk.download('punkt', quiet=True)
 
 
-STOP_WORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",
-              "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's",
-              'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs',
-              'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am',
-              'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
-              'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',
-              'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
-              'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under',
-              'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any',
-              'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than',
-              'too', 'very', 's', 't', 'can', 'will', 'just', 'should', "should've", 'now', 'd', 'll', 'm', 'o',
-              're', 've', 'y', 'ma', 'st', 'nd', 'rd', 'th', "you'll", 'dr', 'mr', 'mrs']
+STOP_WORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're",
+              "you've", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his',
+              'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself',
+              'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom',
+              'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be',
+              'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a',
+              'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at',
+              'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during',
+              'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on',
+              'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when',
+              'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other',
+              'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can',
+              'will', 'just', 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y',
+              'ma', 'st', 'nd', 'rd', 'th', "you'll", 'dr', 'mr', 'mrs']
 
 stemmer = SnowballStemmer('english')
 
 
-def load_pseudo_data(folder_name):
-    """Load ground truth and pseudo data to memory
+# def load_pseudo_data(folder_name):
+#     """Load ground truth and pseudo data to memory
 
-    Parameters:
-        folder_name (str): name of psedo data folder
-        data_file (str): name of train/test data
+#     Parameters:
+#         folder_name (str): name of psedo data folder
+#         data_file (str): name of train/test data
 
-    Returns:
-        (tuple): tupe of length of pseudo data and combined dataframe to test
-    """
+#     Returns:
+#         (tuple): tupe of length of pseudo data and combined dataframe to test
+#     """
 
-    path_list = []
-    for subdir_name in os.scandir(folder_name):
-        for file_name in os.scandir(subdir_name):
-            if file_name.is_file():
-                path_list.append(file_name.path)
+#     path_list = []
+#     for subdir_name in os.scandir(folder_name):
+#         for file_name in os.scandir(subdir_name):
+#             if file_name.is_file():
+#                 path_list.append(file_name.path)
 
-    pseudo_df = pd.concat(map(lambda path: pd.read_csv(
-        path, delimiter='\t', header=None), path_list), ignore_index=True)
-    pseudo_df.columns = ['label', 'review']
+#     pseudo_df = pd.concat(map(lambda path: pd.read_csv(
+#         path, delimiter='\t', header=None), path_list), ignore_index=True)
+#     pseudo_df.columns = ['label', 'review']
 
-    return pseudo_df
+#     return pseudo_df
 
 
 def get_average_word_embeddings(model, tokens):
-    filtered_tokens = [token for token in tokens if token in model.wv.key_to_index]
-    
-    if len(filtered_tokens) > 0:
+    """
+    Calcualte average word embeddings for list of tokens using word vector model.
+
+    Args:
+        model (class): Word vector model
+        tokens (array-like): List of tokens in a document.
+
+    Returns:
+        ndarray: Average word embeddings for the input document.
+    """
+    filtered_tokens = [
+        token for token in tokens if token in model.wv.key_to_index]
+
+    if filtered_tokens:
         return np.mean(model.wv[filtered_tokens], axis=0)
     return np.zeros(model.vector_size)
 
 
-def load_word_vector_model(algo, path):
-    return algo.load(path)
-
-
 def load_torch_model(path):
+    """
+    Load PyTorch model and move to GPU for inference.
+
+    Args:
+        path (str): Model path.
+
+    Returns:
+        Torch.nn.Module: Loaded PyTorch model
+    """
     model = torch.load(path)
     model.cuda()
     model.eval()
@@ -73,22 +90,43 @@ def load_torch_model(path):
     return model
 
 
-def downsampling(label, text):
-    pos_idx = [idx for idx, x in enumerate(label) if x == 1]
-    neg_idx = [idx for idx, x in enumerate(label) if x == 0]
+# #### NEED TO OPTIMIZE
+# def downsampling(label, text):
+#     """
+#     Downsample majority class in binary classification to balance class.
 
-    if len(pos_idx) < len(neg_idx):
-        # no need to shuflle majority since already shuffled in train_test_split
-        downsampled_idx = pos_idx + neg_idx[:len(pos_idx)]
-    else:
-        downsampled_idx = neg_idx + pos_idx[:len(neg_idx)]
+#     Args:
+#         label (list): List of labels.
+#         text (list): List of documents.
 
-    random.shuffle(downsampled_idx)
+#     Returns:
+#         tuple: Downsampled labels and documents.
+#     """
+#     pos_idx = [idx for idx, x in enumerate(label) if x == 1]
+#     neg_idx = [idx for idx, x in enumerate(label) if x == 0]
 
-    return [label[i] for i in downsampled_idx], [text[i] for i in downsampled_idx]
+#     if len(pos_idx) < len(neg_idx):
+#         # no need to shuflle majority since already shuffled in train_test_split
+#         downsampled_idx = pos_idx + neg_idx[:len(pos_idx)]
+#     else:
+#         downsampled_idx = neg_idx + pos_idx[:len(neg_idx)]
+
+#     random.shuffle(downsampled_idx)
+
+#     return [label[i] for i in downsampled_idx], [text[i] for i in downsampled_idx]
 
 
 def train_word_vector_algo(model, texts, path, update=True):
+    """
+    Train word vector algorithm and save it locally.
+
+    Args:
+        model (class): Intialized instance of word vector model. (Either Word2Vec or FastText).
+        texts (list): List of tokens from documents.
+        path (str): Path to save trained model.
+        update (bool, optional): Flag indicating whether to update pretrained model.
+                                Defaults to True.
+    """
     model.build_vocab(texts, update=update)
     model.train(texts,
                 total_examples=model.corpus_count,
@@ -97,17 +135,36 @@ def train_word_vector_algo(model, texts, path, update=True):
 
 
 def tokenize(text):
+    """
+    Clean and tokenize text for processing.
+
+    Args:
+        text (str): Text/Review to be tokenized.
+
+    Returns:
+        list: List of cleaned tokens generated from text.
+    """
+    # Remove URLs, tags.
     text = re.sub(r"http\S+|www\S+|\@\w+", '', text).lower()
     text = re.sub(r"[\n\t\r]", " ", text)
-    # ?, !, . will be sentence stoppers
+    # Replace anything other than alphabets -- ?, !, . will be sentence stoppers -- needed for
+    # sentence tokenization.
     text = re.sub(r'[^a-z.?!]+', ' ', text)
     text = re.sub(r'\.+', '.', text)
-    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
 
     tokens = word_tokenize(text)
-
     return [stemmer.stem(token) for token in tokens if token not in STOP_WORDS]
 
 
 def clean_for_wv(tokens):
+    """
+    Clean unneccesary/meaningless tokens from generated tokens.
+
+    Args:
+        tokens (list): List of tokens from documents.
+
+    Returns:
+        list: List of filtered tokens.
+    """
     return [token for token in tokens if len(token) > 1]
