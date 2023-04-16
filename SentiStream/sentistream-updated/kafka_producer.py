4fea65a0ce1e85650b02d8e84e00f4810ecd23e9
karthick47v2 <karthicksharma11@gmail.com>
2023-04-16 20:24:04 +0800

    fix kafka init

--- SentiStream/sentistream-updated/kafka_producer.py
+++ SentiStream/sentistream-updated/kafka_producer.py
@@ -1,7 +1,35 @@
 # pylint: disable=import-error
+# pylint: disable=no-name-in-module
 
 import csv
 from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
+from kafka.errors import TopicAlreadyExistsError
+
+TOPIC_NAME = 'sentiment-data'
+NUM_PARTITIONS = 1
+REPLICATION_FACTOR = 1
+
+# Initialize KafkaAdminClient with bootstrap servers.
+admin_client = KafkaAdminClient(bootstrap_servers='localhost:9092')
+
+# Delete topic if already created.
+if TOPIC_NAME in admin_client.list_topics():
+    admin_client.delete_topics([TOPIC_NAME])
+
+# Create new topic with desired number of partitions and replication factor.
+new_topic = NewTopic(name=TOPIC_NAME, num_partitions=NUM_PARTITIONS,
+                     replication_factor=REPLICATION_FACTOR)
+
+# Create the topic.
+while True:
+    try:
+        admin_client.create_topics(new_topics=[new_topic])
+        break
+    except TopicAlreadyExistsError:
+        pass
+
+print(f"{TOPIC_NAME} topic has been created.")
 
 
 # Create Kafka producer.
@@ -16,4 +44,4 @@ with open('train.csv', 'r', encoding='utf-8') as file:
 
     # use triple pipe to separate label and text as it is unlikely to occur in text.
     for row in reader:
-        producer.send('sentiment-data', value='|||'.join(row))
+        producer.send(TOPIC_NAME, value=f'{int(row[0]) - 1}|||{row[1]}')
