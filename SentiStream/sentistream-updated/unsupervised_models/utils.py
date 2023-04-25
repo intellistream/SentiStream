7c16bd5e50319f7675087fa5bee5a19be3feb7bc
karthick47v2 <karthicksharma11@gmail.com>
2023-04-25 12:14:49 +0800

    cache frequent vector/words

--- SentiStream/sentistream-updated/unsupervised_models/utils.py
+++ SentiStream/sentistream-updated/unsupervised_models/utils.py
@@ -8,6 +8,9 @@ from Levenshtein import ratio
 
 np.seterr(all='ignore')
 
+txt_cache = {}
+vec_cache = {}
+
 
 def cos_similarity(vec1, vec2):
     """
@@ -15,12 +18,18 @@ def cos_similarity(vec1, vec2):
 
     Args:
         vec1 (ndarray): First word embedding.
-        vec2 (ndarray): Second word embedding.
+        vec2 (ndarray): Ref word embedding.
 
     Returns:
         float: Cosine similarity between 2 vectors.
     """
-    return np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
+    vec2_norm = vec_cache.get(tuple(vec2))
+
+    if vec2_norm is None:
+        vec2_norm = norm(vec2)
+        vec_cache[tuple(vec2)] = vec2_norm
+
+    return np.dot(vec1, vec2) / (norm(vec1) * vec2_norm)
 
 
 def text_similarity(word1, word2, cutoff=0.9):
@@ -38,7 +47,11 @@ def text_similarity(word1, word2, cutoff=0.9):
     temp = []
 
     for word in word2:
-        txt_sim = ratio(word1, word, score_cutoff=cutoff)
+        txt_sim = txt_cache.get((word1, word))
+
+        if txt_sim is None:
+            txt_sim = ratio(word1, word, score_cutoff=cutoff)
+            txt_cache[(word1, word)] = txt_sim
 
         if txt_sim != 0:
             temp.append(txt_sim)
